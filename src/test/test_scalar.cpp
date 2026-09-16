c615cf8e6b6bd47da388538e08274d8fb32b75f4
onedeadlock <saviourme33@gmail.com>
2026-09-16 19:00:31 +0100

    update

--- src/test/test_scalar.cpp
+++ src/test/test_scalar.cpp
@@ -1,3 +1,4 @@
+#include <benchmark/benchmark.h>
 #include "../scalar_impl.hpp"
 
 using namespace httpvo::Implementation;
@@ -6,5 +7,31 @@ int main(void)
 {
     http request;
 
+    return 0;
+}
+
+static void BM_test_1(benchmark::State& state)
+{
+     http test_parse;
+     alignas(8) static char str[] = "GET https://google.com/index HTTP1.1\r\n";
+     std::size_t len = strlen(str);
+
+     for (auto _ : state)
+     {
+        int res = test_parse.parse_header_line_sc(str, len, len);
+        benchmark::DoNotOptimize(res);
+     }
+}
+
+BENCHMARK(BM_test_1);
+
+int main(int argc, char **argv)
+{
+    benchmark::MaybeReenterWithoutASLR(argc, argv);
+    benchmark::Initialize(&argc, argv);
+    if (benchmark::ReportUnrecognizedArguments(argc, argv))
+        return 1;
+    benchmark::RunSpecifiedBenchmarks();
+    benchmark::Shutdown();
     return 0;
 }
